a4db8e0e2fd5b74b33f0884bab9ff5ea3443f310
Zhe-Rui, Yang <zhreuiyang@gmail.com>
2018-12-21 12:24:26 +0800

    lazySign現在可以直接在文字互動式介面新增或刪除資料庫的人員資料了

--- lazySign.py
+++ lazySign.py
@@ -1,35 +1,107 @@
 #! python3
 # auto sign in serve table
 
-import pyautogui, time, sched, os, shelve, sys
+import pyautogui, time, sched, os, shelve, sys, re
 
 def interface(window=True, _schedule=None):
+    picPath = os.path.join(os.environ['USERPROFILE'], 'AppData\Local\lazySign')
+    pyPath = r'path\to\python.exe'##############################################################
+    pyw = r'path\to\pythonw.exe' ###############################################################
+    scrPa = r'path\to\this\script.py' ###############################################################
     try:
         _schedule = sys.argv[1]
         for i in _schedule.split(';'):
             preOrder(i)
     except:
-        a = []
+        _dataBox = []
         while True:
             if window: # open a window to collecting imformations
-                a.append(input('輸入人員代號及上值班台時間: (e.g. ○○○,10,20) '))
-                _pause = input('輸入完成？ (輸入「y」以檢查輸入的資料；其餘任意鍵以繼續輸入。)')
-                if _pause == 'y':
-                    print('目前已輸入的資料：')
-                    for i in a:
-                        print(i)
-                    print('')
-                    _excu = input('確認無誤後輸入「go」以開始；其餘任何鍵以繼續輸入。') # delete?
-                    if _excu == 'go':
-                        window = False
+                print('┌──────────────┐ 　 パ..パ')
+                print('│歡迎使用值班台登入排程小幫手│　（ ° ∀ ° ）')
+                print('└──────────────┘＿（＿b /￣￣￣￣￣/＿')
+                print('')
+                _text = input('輸入人員代號及上值班台時間；直接按enter以跳過；輸入「member」以進入新增/刪除人員模式： (e.g. ○○○,10,20) ')
+                if _text == 'member':
+                    _switch = input('輸入「new」以新增人員資料；輸入「del」以刪除人員資料；輸入其餘任何鍵以回到上一層。')
+                    if _switch == 'new':
+                        while True:
+                            _newmember = []
+                            _newmember.append(input('輸入人員代號：(如不想新增，直接enter以退回上一層)：'))
+                            if _newmember[0] = '':
+                                break
+                            _newmember.append(input('輸入交接班作業的下拉選單要按幾次下鍵才能正確指定：'))
+                            _newmember.append(input('輸入登入密碼：(注意：此程式會永久記錄此密碼直到手動刪除！)'))
+                            if not isinstance(_newmember[1], int) or not re.match(r'^[A-Za-z](\d){9}$', _newmember[2]):
+                                if not isinstance(_newmember[1], int):
+                                    print('交接班作業的下拉選單部分必須要是數字，請全部重新輸入。')
+                                else:
+                                    print(f'您輸入的登入密碼「{_newmember[2]}」不符合格式，請全部重新輸入。')
+                            else:
+                                print(f'您所設定的人員代號為{_newmember[0]}；交接班作業下拉選單的下鍵指定次數為{_newmember[1]}；登入密碼為{_newmember[2]}。')
+                                _memberDataCheck = input('確認無誤後輸入「ok」將資料存入電腦；輸入其餘任何鍵重新輸入：')
+                                if re.match(r'(ok|OK|Ok|oK|okey|Okey|okay|Okay|okey-doke)', _memberDataCheck):
+                                    with shelve.open(picPath + '\PID') as id:
+                                        id[f'{_newmember[0]}'] = _newmember[2]
+                                        id[f'{_newmember[0]}_KeyPressTimes'] = _newmember[1]
+                                    os.system('pause >nul | echo 資料儲存完成，按下任何鍵以回上一層。')
+                                    break
+                                else:
+                                    continue
+                    elif _switch == 'del':
+                        while True:
+                            _useless = input('輸入要刪除的人員代號以刪除該筆資料；輸入「?」以閱覽所有的人員代號；輸入「q」離開刪除模式：')
+                            if _useless == '?':
+                                with shelve.open(picPath + '\PID') as id:
+                                    for i in id.keys():
+                                        print(i)
+                            elif _useless == 'q':
+                                os.system(f'{pyPath} {scrPa}')
+                                os._exit()
+                            else:
+                                with shelve.open(picPath + '\PID') as id:
+                                    if not _useless in id:
+                                        print('資料庫中沒有這個人員代號。')
+                                    else:
+                                        delCheck = input(f'確認刪除{_useless}？ (輸入對應的登入密碼以確認刪除；輸入「n」取消。)')  
+                                        elif delCheck == id[_useless]:
+                                            del id[_useless]
+                                            del id[f'{_useless}_KeyPressTimes']
+                                            os.system('pause >nul | echo 成功刪除{_useless}！按下任何鍵以繼續。')
+                                        elif delCheck == 'n':
+                                            continue
+                                        else:
+                                            print('密碼錯誤，請再次確認。')
                     else:
                         continue
+                elif re.match(r'(.*)(,(\d|\d\d))+'):
+                    _dataBox.append(_text)
+                else:
+                    print(f'你輸入的字串是「{_text}」。該字串格式有誤，請重新輸入。')
+                    continue
+                _pause = input('輸入完成？ (輸入「y」以檢查輸入的資料；輸入其餘任意鍵以繼續輸入。)')
+                if _pause == 'y':
+                    while True:
+                        print('目前已輸入的資料：')
+                        for i in _dataBox:
+                            print(i)
+                        print('')
+                        _excu = input('確認無誤後輸入「go」以開始；輸入「d」以刪除已輸入的資料；輸入其餘任何鍵以繼續輸入。')
+                        if _excu == 'go':
+                            window = False
+                            break
+                        elif _excu == 'd':
+                            _d = input('輸入人員代號以刪除該行資料：(e.g. 欲刪除「abc,08,18」這行請輸入「abc」) ')
+                            for i in _dataBox:
+                                try:
+                                    del _dataBox[re.match(f'{_d}.*').group()]
+                                except:
+                                    continue
+                        else:
+                            break
             else: # run the main process without window
-                f = ';'.join(a)
-                pyw = r'path\to\pythonw.exe' ###############################################################
-                scrpa = r'path\to\script.py' ###############################################################
+                _dataString = ';'.join(_dataBox)
                 os.system('pause >nul | echo 排程已記錄，按下任意鍵以轉入背景運行...')
-                os.system(f'{pyw} {scrpa} {f}')
+                os.system(f'{pyw} {scrPa} {_dataString}')
                 break
 
 def preOrder(t):
@@ -48,8 +120,8 @@ def epochTime(h, end=False):
         return time.mktime((int(dayString[:4]), int(dayString[4:6]), int(dayString[6:]), int(h), 30, 18, 0, 0, -1))
     
 def signIn(name):
-    # locate app
     picPath = os.path.join(os.environ['USERPROFILE'], 'AppData\Local\lazySign')
+    # locate app
     counter = 0
     while True:
         if not pyautogui.locateCenterOnScreen(os.path.join(picPath, '交接班管理作業.png')):
@@ -67,10 +139,9 @@ def signIn(name):
             break
         else:
             continue
-    downPressTimes = {'ZRY': 7}
-    pyautogui.press('down', presses=downPressTimes[f'{name}'], interval=0.1)
-    pyautogui.typewrite('\t')
-    with shelve.open(picPath + '\PID') as id:
+    with shelve.open(picPath + '\PID') as id: ###################################################################
+        pyautogui.press('down', presses=int(id[f'{name}_KeyPressTimes']), interval=0.2)
+        pyautogui.typewrite('\t')
         pyautogui.typewrite(id[f'{name}'], interval=0.1)
     pyautogui.click(pyautogui.locateCenterOnScreen(os.path.join(picPath, '確定.png')), interval=1)
     time.sleep(5)
